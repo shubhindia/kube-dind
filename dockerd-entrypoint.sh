381eae9e8d0d33076526a7f9f6c5577cbcd73596
shubhindia <shubhindia123@gmail.com>
2020-10-04 11:52:18 +0530

    its not an array anymore
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -2,7 +2,7 @@
 
 _get_kubeconfig(){
 KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/gitlab-runner/configmaps/kubeconfig | jq -j ".items[0].data.kubeconfig" > /kubeconfig
+curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/gitlab-runner/configmaps/kubeconfig | jq -j ".data.kubeconfig" > /kubeconfig
 }
 
 _get_kubeconfig
